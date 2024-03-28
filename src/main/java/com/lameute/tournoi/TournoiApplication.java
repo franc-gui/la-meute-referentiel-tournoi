f07f2aaab0efacf12977ba1d22c01566261916a2
Franc-Guillaume van der Goes <borneheld@gmail.com>
2024-03-28 11:37:36 +0100

    :sparkles: Base projet

---
+++ src/main/java/com/lameute/tournoi/TournoiApplication.java
@@ -0,0 +1,13 @@
+package com.lameute.tournoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lameute/tournoi/TournoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lameute.tournoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
